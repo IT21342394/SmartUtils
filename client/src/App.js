f300491ba256e1ff1cff5b36296eec2870f6f231
Shashi Vinod <99243796+IT21288326@users.noreply.github.com>
2025-04-11 19:03:45 +0530

    project initialization

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to SmartUtils</h1>
+      <p>Mini Tools Hub (MERN Stack)</p>
+    </div>
+  );
+}
+
+export default App;
