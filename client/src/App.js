ba83ec64ba1891cd44ebc8f107b9bd02c2143989
Shashi Vinod <99243796+IT21288326@users.noreply.github.com>
2025-04-14 00:32:52 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom'
 import SmartUnitConverter from './Screens/UnitConverter/SmartUnitConverter';
-import AdvancedBMICalculator from './Screens/BMICalculator/AdvancedBMICalculator';
+
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
         <Route path="/" element={<SmartUnitConverter/>} />
-        <Route path="/BmiCal" element={<AdvancedBMICalculator/>} />
+        
         </Routes>
       </BrowserRouter>
     </div>
